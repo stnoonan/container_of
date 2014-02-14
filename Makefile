703cf2445d8b53854e7cb13170e35f51fcab1131
Sean Timothy Noonan <stnoonan@obsolescence.net>
2014-02-14 15:49:19 -0500

    Rename example and add to makefile

--- Makefile
+++ Makefile
@@ -9,13 +9,16 @@ test_opaque: opaque.o
 test_public: public.o
 	${CC} ${CFLAGS} -o bin/test_public test/test_public.c public.o
 
+example: opaque.o public.o
+	${CC} ${CFLAGS} -o bin/example test/example.c public.o opaque.o
+
 opaque.o:
 	${CC} ${CFLAGS} -c -o opaque.o opaque/opaque.c
 
 public.o:
 	${CC} ${CFLAGS} -c -o public.o public/public.c
 
-all: test
+all: test example
 
 clean:
 	rm -f bin/* *.o
--- test/example.c
+++ test/example.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#include "public.h"
+#include "opaque_public.h"
+#include "public_public.h"
 
 void test_opaque() {
     opaque_container_t *opaque = init_counter_o();
     increment_counter_o(opaque);
     tag_counter_o(opaque, "opaque");
-    fprintf(stderr, "Opaque counter: %lu\n", read_counter_o(opaque));
+    fprintf(stderr, "Opaque counter: %"PRIu64"\n", read_counter_o(opaque));
 }
 
 void test_public() {
     public_data_t *pub = init_counter_c();
     ++pub->counter;
     tag_counter_c(pub, "public");
-    fprintf(stderr, "Public counter: %lu\n", pub->counter);
+    fprintf(stderr, "Public counter: %"PRIu64"\n", pub->counter);
 }
 
 int main(int argc, char** argv) {
